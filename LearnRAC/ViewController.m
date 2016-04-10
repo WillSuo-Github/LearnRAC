edf3b3c4565c1bbbb430cf331dd40e13165f7520
zuihuodeman <739002611@qq.com>
2016-04-10 21:08:24 +0800

    sss

--- LearnRAC/ViewController.m
+++ LearnRAC/ViewController.m
@@ -21,6 +21,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+//    NSArray *arr = @[@1,@2,@3];
+//    
+//    [arr.rac_sequence.signal subscribeNext:^(id x) {
+//        NSLog(@"%@",x[1]);
+//    }];
+    
+//    NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:@"1",@"a",@"2",@"b",@"3",@"c", nil];
+//    [dict.rac_sequence.signal subscribeNext:^(id x) {
+////        NSLog(@"%@",x[1]);
+//        RACTupleUnpack(NSString *key,id value) = x;
+//        
+//        NSLog(@"%@,%@",key,value);
+//    }];
+    
+    
     
     
 }
@@ -29,16 +44,16 @@
 - (IBAction)chick:(id)sender {
     
     
-    RACReplaySubject *subject = [RACReplaySubject subject];
-    
-    [subject sendNext:@"111"];
-    [subject subscribeNext:^(id x) {
-        NSLog(@"第一个订阅者%@",x);
-    }];
-    
-    [subject subscribeNext:^(id x) {
-        NSLog(@"第二个订阅者%@",x);
-    }];
+//    RACReplaySubject *subject = [RACReplaySubject subject];
+//    
+//    [subject sendNext:@"111"];
+//    [subject subscribeNext:^(id x) {
+//        NSLog(@"第一个订阅者%@",x);
+//    }];
+//    
+//    [subject subscribeNext:^(id x) {
+//        NSLog(@"第二个订阅者%@",x);
+//    }];
     
     
     
